8924dab943b894f2ad15dfd1628e09a4d633fa47
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-10-15 17:53:26 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,45 @@
 
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  colorArea: document.querySelector('body'),
+};
+
+const { startBtn, stopBtn, colorArea } = refs;
+let intervalId = null;
+let isActive = false;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+stopBtn.setAttribute('disabled', 'true');
+
+function onStartBtnClick() {
+  if (!isActive) {
+    intervalId = setInterval(() => {
+      let currentColor = getRandomHexColor();
+
+      colorArea.style.backgroundColor = currentColor;
+    }, 1000);
+
+    startBtn.setAttribute('disabled', 'true');
+    stopBtn.removeAttribute('disabled');
+
+    isActive = true;
+  }
+}
+
+function onStopBtnClick() {
+  if (isActive) {
+    clearInterval(intervalId);
+
+    isActive = false;
+  }
+
+  stopBtn.setAttribute('disabled', 'true');
+  startBtn.removeAttribute('disabled');
+}
